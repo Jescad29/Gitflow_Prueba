764e5acd838d5b4302b37f5fc919702e527b628a
JesCad <jesuscadena27@hotmail.com>
2023-06-09 16:24:04 -0600

    Aplicacion de Gitflow

--- gitflow2.py
+++ gitflow2.py
@@ -1,3 +1,4 @@
 print("Creacion de fichero en rama main gitflow2.py y modificacion")
 print("Prueba para subir cambios a github")
 print ("Aplicacion de git fetch")
+print ("Comienzo de aplicacion de Gitflow")
